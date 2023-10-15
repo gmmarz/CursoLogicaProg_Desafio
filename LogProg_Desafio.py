f92800e95dfafe29555febb1742ff034a2b660f6
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-10-15 01:36:53 -0300

    First commit

---
+++ LogProg_Desafio.py
@@ -0,0 +1,5 @@
+#Curso de lógica de programação 
+#Desafio: Desenvolver uma aplicação simples para praticar os conceitos aprendidos: for, while , input e print.
+#O operador de caixa de uma loja, cadastra cada cliente que comprar dados:  - Nome - Telefone e Endereço
+#O fim do dia, quando operador digitar Fim termina o cadastro dos clientes.
+#Sistem deve então realizar o sorteiro e um dos clientes, e informar qual o Nome do cliente sorteado e seu telefone.
